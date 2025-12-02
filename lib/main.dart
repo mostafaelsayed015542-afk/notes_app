5032dc6f518309f827160a7058d6e344744098b0
Mostafa Elsayed <your-email@example.com>
2025-12-02 20:04:17 +0200

    init hive and add notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Views/home_page.dart';
+import 'package:notes_app/widgets/constant.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(knotebox);
   runApp(const MyApp());
 }
 
--- lib/widgets/constant.dart
+++ lib/widgets/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 Color kprimarycolor = Color(0xff62FCD7);
+const knotebox='notes_box';
